01754a4c2009cee061eb5de11c64a396b2048756
Sam Stifter <sam.stifter@gmail.com>
2019-11-22 14:50:38 -0600

    Beginning of the a star implementation
    Co-authored-by: rgoluch <rgoluch@iastate.edu>
    Co-authored-by: druner@iastate.edu <druner@iastate.edu>

--- dungeon_game.py
+++ dungeon_game.py
@@ -182,6 +182,78 @@ class Monster(Tile):
 
     #def chasePlayer(self):
 
+def reconstruct_path(cameFrom, current):
+    total_path := {current}
+    while current in cameFrom.Keys:
+        current := cameFrom[current]
+        total_path.prepend(current)
+    return total_path
+
+# def d():
+
+
+
+def cooler_A_Star(start, goal):
+    # 1. make a copy of the dungeon except its all numbers and they r all 0
+    copy = [[0] * len(my_dungeon.grid[0]) for i in range(len(my_dungeon.grid))]
+
+    # 2. set the spot the monster is at to some non 0 number, it can be literally anything
+    copy[start.x][start.y] = 100
+    # 3. iterate this next part a ton of times
+    
+    for i in range(100):
+        #   4. for everything in the dungeon that is not a 0, add in numbers in every direction of it, only for the spots that are air in the "real" dungeon
+        for y in range(len(my_dungeon.grid)):
+            for x in range(len(my_dungeon.grid[0])):
+                
+
+                #   5. if you changed a spot that in the "real" dungeon is the player, stop
+                if(y==goal.y and x==goal.x):
+                    break
+    # 6. trace the path back and return the first move the monster should make
+
+
+### BEGIN PSEUDO CODE
+# A* finds a path from start to goal.
+# h is the heuristic function. h(n) estimates the cost to reach goal from node n.
+def A_Star(start, goal, h):
+    # The set of discovered nodes that may need to be (re-)expanded.
+    # Initially, only the start node is known.
+    openSet = {start}
+
+    # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
+    cameFrom = {}# an empty map
+
+    # For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
+    gScore = {} #map with default value of Infinity
+    gScore[start] = 0
+
+    # For node n, fScore[n] := gScore[n] + h(n).
+    fScore = {} #map with default value of Infinity
+    fScore[start] = h(start)
+
+    while openSet is not empty
+        current := the node in openSet having the lowest fScore[] value
+        if current = goal
+            return reconstruct_path(cameFrom, current)
+
+        openSet.Remove(current)
+        for each neighbor of current
+            # d(current,neighbor) is the weight of the edge from current to neighbor
+            # tentative_gScore is the distance from start to the neighbor through current
+            tentative_gScore := gScore[current] + d(current, neighbor)
+            if tentative_gScore < gScore[neighbor]
+                # This path to neighbor is better than any previous one. Record it!
+                cameFrom[neighbor] := current
+                gScore[neighbor] := tentative_gScore
+                fScore[neighbor] := gScore[neighbor] + h(neighbor)
+                if neighbor not in openSet
+                    openSet.add(neighbor)
+
+    # Open set is empty but goal was never reached
+    return failure
+
+##### END PSEUDO CODE
 
 pygame.init()
 screen = pygame.display.set_mode((DUNGEON_SIZE*TILE_SIZE, DUNGEON_SIZE*TILE_SIZE))
